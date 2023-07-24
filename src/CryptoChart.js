2324cfc01b45e9e39e06051dead0e2a3bfaf7970
Panda77 <PiperZzz@users.noreply.github.com>
2023-07-24 11:01:41 -0400

    Set visible range

--- src/CryptoChart.js
+++ src/CryptoChart.js
@@ -21,6 +21,11 @@ function CryptoChart({ data }) {
 
         lineSeries.setData(data);
 
+        // Set visible range to start from the first data point
+        const from = data[0].time;
+        const to = data[data.length - 1].time;
+        chart.timeScale().setVisibleRange({ from, to });
+
         return () => chart.remove();
     }, [data]);
 
